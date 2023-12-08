fc642ddf20456d874f988b3cb9abf00ff0ca5f46
Sebastian Arana <52842507+SebasProgrammer@users.noreply.github.com>
2023-12-09 00:00:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,19 +11,26 @@ frame_placeholder = st.empty()
 
 stop_button_pressed = st.button("Stop")
 
-while cap.isOpened() and not stop_button_pressed:
+option = st.selectbox(
+        'Selecciona tarea a realizar',
+        ('None', 'Prender cmara', 'Upload photo'))
     
-    ret, frame = cap.read()
-    if not ret:
-        st.write("The video capture has ended.")
-        break
+    # Start with app logic:
+if option == 'Prender camara':
 
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    while cap.isOpened() and not stop_button_pressed:
+        
+        ret, frame = cap.read()
+        if not ret:
+            st.write("The video capture has ended.")
+            break
 
-    frame_placeholder.image(frame, channels="RGB")
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    if cv2.waitKey(1) & 0xFF == ord("q") or stop_button_pressed:
-        break
+        frame_placeholder.image(frame, channels="RGB")
+
+        if cv2.waitKey(1) & 0xFF == ord("q") or stop_button_pressed:
+            break
 
 cap.release()
 cv2.destroyAllWindows()
