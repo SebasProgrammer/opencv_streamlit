ab2dad6ee437546776adb507cdd3a153cee8cdc6
Sebastian Arana <52842507+SebasProgrammer@users.noreply.github.com>
2023-12-09 00:53:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import av
-from streamlit_webrtc import webrtc_streamer
+from streamlit_webrtc import webrtc_streamer, RTCConfiguration
 
 class VideoProcessor:
     def recv(self, frame):
@@ -7,5 +7,8 @@ class VideoProcessor:
 
         return av.VideoFrame.from_ndarray(img, format="bgr4")
 
-webrtc_streamer(key="key", video_processor_factory=VideoProcessor)
+webrtc_streamer(key="key", video_processor_factory=VideoProcessor,
+                rtc_configuration=RTCConfiguration(
+                    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+                ))
 
