7ac466f210bc5d63c192728cf2afbd70571fff7f
Sebastian Arana <52842507+SebasProgrammer@users.noreply.github.com>
2023-12-09 00:43:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import cv2
-import streamlit as st
-import numpy as np
-import tempfile
 import av
 from streamlit_webrtc import webrtc_streamer
 
@@ -11,17 +7,5 @@ class VideoProcessor:
 
         return av.VideoFrame.from_ndarray(img, format="bgr4")
 
-st.title("Prueba")
+webrtc_streamer(key="key", video_processor_factory=VideoProcessor)
 
-frame_placeholder = st.empty()
-
-stop_button_pressed = st.button("Stop")
-
-option = st.selectbox(
-        'Selecciona tarea a realizar',
-        ('None', 'Prender camara', 'Upload photo'))
-
-    # Start with app logic:
-if option == 'Prender camara':
-    webrtc_streamer(key="key", video_processor_factory=VideoProcessor)
-    # cap = cv2.VideoCapture(0)
