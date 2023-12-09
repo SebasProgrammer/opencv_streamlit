ab0e85615b00733b19309a36ada7c1192574c43f
Sebastian Arana <52842507+SebasProgrammer@users.noreply.github.com>
2023-12-09 01:07:41 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,8 @@ import streamlit as st
 import av
 
 def callback(frame: av.VideoFrame) -> av.VideoFrame:
-    return frame
+    img = frame.to_ndarray(format="bgr24")
+    return av.VideoFrame.from_ndarray(img, format="bgr24")
 st.title("New APP")
 
 webrtc_streamer(key="key", video_frame_callback=callback)
